b0f9d7774a4335b31ccfdb7745f8a971a2324aaa
Patrick Genevich <patrickgenevich@gmail.com>
2021-05-09 09:55:07 -0700

    inital commit

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt } from "@graphprotocol/graph-ts";
 import {
   Contract,
   BuyerUpdated,
@@ -6,59 +6,42 @@ import {
   ProductPurchased,
   RoleAdminChanged,
   RoleGranted,
-  RoleRevoked
-} from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+  RoleRevoked,
+} from "../generated/Contract/Contract";
+import { Buyer, Product, Purchase, Seller } from "../generated/schema";
 
 export function handleBuyerUpdated(event: BuyerUpdated): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.buyerAddress = event.params.buyerAddress
-  entity.metadata = event.params.metadata
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.ADMIN_ROLE(...)
-  // - contract.DEFAULT_ADMIN_ROLE(...)
-  // - contract.getRoleAdmin(...)
-  // - contract.hasRole(...)
-  // - contract.supportsInterface(...)
+  let entity = new Buyer(event.transaction.from.toHex());
+  entity.metadata = event.params.metadata;
+  entity.save();
 }
 
-export function handleProductCreated(event: ProductCreated): void {}
+export function handleProductCreated(event: ProductCreated): void {
+  let entity = new Product(event.params.productId.toHex());
+  entity.metadata = event.params.metadata;
+  entity.price = event.params.price;
+  entity.seller = event.params.sellerAddres.toHex();
+  entity.createdOn = event.block.timestamp;
+  entity.save();
 
-export function handleProductPurchased(event: ProductPurchased): void {}
+  let seller = new Seller(event.params.sellerAddres.toHex());
+  seller.save();
+}
+
+export function handleProductPurchased(event: ProductPurchased): void {
+  let entity = new Purchase(event.transaction.hash.toHex());
+  entity.timestamp = event.block.timestamp;
+  entity.product = event.params.productId.toHex();
+  entity.buyer = event.params.buyer.toHex();
+
+  let product = Product.load(event.params.productId.toHex());
+  entity.seller = product.seller;
+
+  entity.save();
+
+  let buyer = new Buyer(event.params.buyer.toHex());
+  buyer.save();
+}
 
 export function handleRoleAdminChanged(event: RoleAdminChanged): void {}
 
